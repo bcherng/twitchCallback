8a7e1ff0d1b7b72abd24dc0d48cc2c51e92a3804
bcherng <briancherngsch@gmail.com>
2023-11-20 20:57:33 -0800

    test

--- app.js
+++ app.js
@@ -2,10 +2,83 @@ const crypto = require('crypto')
 const express = require('express');
 const app = express();
 const port = 443;
-const path = require('path')
-app.use('/static', express.static(path.join(__dirname, 'public')))
+const path = require('path');
+const tmi = require('tmi.js');
+const ratge = {
+    stars:0
+};
+
+const successRates = [
+    0.95,   //0
+    0.9,    //1
+    0.85,   //2
+    0.85,   //3
+    0.8,    //4
+    0.75,   //5
+    0.70,   //6
+    0.65,   //7
+    0.60,   //8
+    0.55,   //9
+    0.50,   //10
+    0.45,   //11
+    0.40,   //12
+    0.35,   //13
+    0.30,   //14
+    0.30,   //15
+    0.30,   //16
+    0.30,   //17
+    0.30,   //18
+    0.30,   //19
+    0.30,   //20
+    0.30,   //21
+    0.03,   //22
+    0.02,    //23
+    0.01,    //24
+];
+const decreaseRates = [
+    0,   //0
+    0,    //1
+    0,   //2
+    0,   //3
+    0,    //4
+    0,   //5
+    0,   //6
+    0,   //7
+    0,   //8
+    0,   //9
+    0,   //10
+    0,   //11
+    0,   //12
+    0,   //13
+    0,   //14
+    0,   //15
+    0.97,   //16
+    0.97,   //17
+    0.96,   //18
+    0.97,   //19
+    0,   //20
+    0.9,   //21
+    0.8,   //22
+    0.7,    //23
+    0.6,    //24
+]
+
+const opts = {
+    identity: {
+      username: 'yogBot',
+      password: '23e27e6eip8oq7mk8yxcftc6ur38in'
+    },
+    channels: [
+      'kahyo_gms'
+    ]
+  };
+  
+const client = new tmi.client(opts);
 
 let counter = 0;
+
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
 
 // Notification request headers
 const TWITCH_MESSAGE_ID = 'Twitch-Eventsub-Message-Id'.toLowerCase();
@@ -28,6 +101,55 @@ app.use(express.raw({          // Need raw message body for signature verificati
 app.get("/", (req,res) => {
     res.setHeader('Content-Type', 'text/html');
     res.end("The counter is: " + counter);
+})
+app.post('/starforce', (req, res) => {
+    console.log(req);
+    let secret = getSecret();
+    let message = getHmacMessage(req);
+    let hmac = HMAC_PREFIX + getHmac(secret, message);  // Signature to compare
+
+    if (true === verifyMessage(hmac, req.headers[TWITCH_MESSAGE_SIGNATURE])) {
+        console.log("signatures match");
+
+        // Get JSON object from body, so you can process the message.
+        let notification = JSON.parse(req.body);
+        
+        if (MESSAGE_TYPE_NOTIFICATION === req.headers[MESSAGE_TYPE]) {
+            if (Math.floor(Math.random() * 100) + 1 < successRates[ratge.stars]) {
+                ratge.stars += 1;
+                client.say('kahyo_gms', "Sucess! Ratge is now " + ratge.stars + " stars");
+            } else if (Math.floor(Math.random() * 100) + 1 < decreaseRates[ratge.stars]) {
+                ratge.stars -= 1;
+                client.say('kahyo_gms', "Failure... Ratge is now " + ratge.stars + " stars");
+            } else {
+                ratge.stars = 12;
+                client.say('kahyo_gms', "Destroyed. Ratge is back to 12 stars");
+            }
+
+            console.log(`Event type: ${notification.subscription.type}`);
+            console.log(JSON.stringify(notification.event, null, 4));
+            
+            res.sendStatus(204);
+        }
+        else if (MESSAGE_TYPE_VERIFICATION === req.headers[MESSAGE_TYPE]) {
+            res.set('Content-Type', 'text/plain').status(200).send(notification.challenge);
+        }
+        else if (MESSAGE_TYPE_REVOCATION === req.headers[MESSAGE_TYPE]) {
+            res.sendStatus(204);
+
+            console.log(`${notification.subscription.type} notifications revoked!`);
+            console.log(`reason: ${notification.subscription.status}`);
+            console.log(`condition: ${JSON.stringify(notification.subscription.condition, null, 4)}`);
+        }
+        else {
+            res.sendStatus(204);
+            console.log(`Unknown message type: ${req.headers[MESSAGE_TYPE]}`);
+        }
+    }
+    else {
+        console.log('403');    // Signatures didn't match.
+        res.sendStatus(403);
+    }
 })
 
 app.post('/eventsub', (req, res) => {
